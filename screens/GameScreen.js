091926586eda22471c0a4b14a557ee2dcee65039
Brandon Leal <bleal@cgtscorp.com>
2023-07-07 13:19:27 -0400

    add main screens

---
+++ screens/GameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const GameScreen = () => {
+  return (
+    <View>
+      <Text>GameScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default StartGameScreen;
