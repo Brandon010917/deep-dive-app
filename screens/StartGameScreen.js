99216cef3241e19ead8ae2fe71771ff32c3d15d0
Brandon Leal <bleal@cgtscorp.com>
2023-07-10 10:27:32 -0400

    add screen StartGame and add validations

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,13 +1,95 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput, View, Alert } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const resetEnteredNumberHandler = () => {
+    setEnteredNumber("");
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: resetEnteredNumberHandler,
+          },
+        ]
+      );
+      return;
+    }
+
+    console.log('Valid Number');
+  };
+
   return (
-    <View>
-      <Text>StartGameScreen</Text>
+    <View style={styles.inputcontainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={setEnteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetEnteredNumberHandler}>
+            Reset
+          </PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputcontainer: {
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#3b021f",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    alignItems: "center",
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
 
 export default StartGameScreen;
